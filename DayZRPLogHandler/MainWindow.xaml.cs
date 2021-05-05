0c85ebe3f628e8614ee536e4a1f1670151babba1
ryan broman <ryanjbroman@gmail.com>
2021-05-05 00:08:02 -0700

    1.0

---
+++ DayZRPLogHandler/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace DayZRPLogHandler {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        private Parser _parser = new();
+        private readonly TextBox[] _logs;
+
+        public MainWindow() {
+            InitializeComponent();
+            _logs = new[] {KillLogs, HitLogs, ChatLogs, BuildingLogs, PositionLogs, ConnectionLogs};
+        }
+
+        private void UploadButtonClick(object sender, RoutedEventArgs e) {
+            _parser = new Parser();
+            var upload = new OpenFileDialog {Filter = "DayZ admin log files (*.ADM)|*.ADM;|Text files (*.txt)|*.txt"};
+            if(upload.ShowDialog() != true) return;
+            FilePathBox.Text = upload.FileName;
+            _parser.Parse(upload.FileName);
+
+            foreach(var log in _logs) {
+                log.Text = "";
+            }
+
+            KillLogs.Text = string.Join("\n", _parser.Kills);
+            HitLogs.Text = string.Join("\n", _parser.Hits);
+            ChatLogs.Text = string.Join("\n", _parser.Chats);
+            BuildingLogs.Text = string.Join("\n", _parser.Building);
+            PositionLogs.Text = string.Join("\n", _parser.Position);
+            ConnectionLogs.Text = string.Join("\n", _parser.Connection);
+
+            IDBox.IsEnabled = true;
+            PosBox.IsEnabled = true;
+            SearchButton.IsEnabled = true;
+            SearchText.IsEnabled = true;
+        }
+
+        private void SearchButtonClick(object sender, RoutedEventArgs e) {
+            Search(SearchText.Text);
+        }
+
+        private void Search(string text) {
+            SetToDefault();
+            if(IDBox.IsChecked == true) {
+                HideIDs();
+            }
+            else if(PosBox.IsChecked == true) {
+                HidePos();
+            }
+
+            foreach(var log in _logs) {
+                if(log.Equals(ChatLogs)) {
+                    // Explicit exception to the double-space removal for chat logs.
+                    log.Text = string.Join("\n", log.Text.Split('\n').Where(s => s.ToLower().Contains(text.ToLower())));
+                }
+
+                log.Text = string.Join("\n", log.Text.Split('\n').Where(s => s.ToLower().Contains(text.ToLower())))
+                    .Replace("  ", " ");
+            }
+        }
+
+        private void HideIDs(object sender, RoutedEventArgs routedEventArgs) {
+            HideIDs();
+            Search(SearchText.Text);
+        }
+
+        private void HideIDs() {
+            if(PosBox.IsChecked == true) {
+                NoIDNoPos();
+            }
+            else {
+                NoID();
+            }
+        }
+
+        private void UnhideIDs(object sender, RoutedEventArgs e) {
+            UnhideIDs();
+            Search(SearchText.Text);
+        }
+
+        private void UnhideIDs() {
+            if(PosBox.IsChecked == true) {
+                NoPos();
+            }
+            else {
+                SetToDefault();
+            }
+        }
+
+        private void HidePos(object sender, RoutedEventArgs e) {
+            HidePos();
+            Search(SearchText.Text);
+        }
+
+        private void HidePos() {
+            if(IDBox.IsChecked == true) {
+                NoIDNoPos();
+            }
+            else {
+                NoPos();
+            }
+        }
+
+        private void UnhidePos(object sender, RoutedEventArgs e) {
+            UnhidePos();
+            Search(SearchText.Text);
+        }
+
+        private void UnhidePos() {
+            if(IDBox.IsChecked == true) {
+                NoID();
+            }
+            else {
+                SetToDefault();
+            }
+        }
+
+        private void SetToDefault() {
+            KillLogs.Text = string.Join("\n", _parser.Kills);
+            HitLogs.Text = string.Join("\n", _parser.Hits);
+            ChatLogs.Text = string.Join("\n", _parser.Chats);
+            BuildingLogs.Text = string.Join("\n", _parser.Building);
+            PositionLogs.Text = string.Join("\n", _parser.Position);
+            ConnectionLogs.Text = string.Join("\n", _parser.Connection);
+        }
+
+        private void NoID() {
+            KillLogs.Text = string.Join("\n", _parser.KillsNoID);
+            HitLogs.Text = string.Join("\n", _parser.HitsNoID);
+            ChatLogs.Text = string.Join("\n", _parser.ChatsNoID);
+            BuildingLogs.Text = string.Join("\n", _parser.BuildingNoID);
+            PositionLogs.Text = string.Join("\n", _parser.PositionNoID);
+            ConnectionLogs.Text = string.Join("\n", _parser.ConnectionNoID);
+        }
+
+        private void NoIDNoPos() {
+            KillLogs.Text = string.Join("\n", _parser.KillsNoIDNoPos);
+            HitLogs.Text = string.Join("\n", _parser.HitsNoIDNoPos);
+            BuildingLogs.Text = string.Join("\n", _parser.BuildingNoIDNoPos);
+        }
+
+        private void NoPos() {
+            KillLogs.Text = string.Join("\n", _parser.KillsNoPos);
+            HitLogs.Text = string.Join("\n", _parser.HitsNoPos);
+            BuildingLogs.Text = string.Join("\n", _parser.BuildingNoPos);
+        }
+    }
+}
---
+++ DayZRPLogHandler/Parser.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Parser.cs
+ * Created by Ryan Broman on 2021-04-24
+ * ryan@broman.dev
+ */
+
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DayZRPLogHandler {
+    public class Parser {
+        public ObservableCollection<string> Kills { get; } = new();
+        public ObservableCollection<string> Hits { get; } = new();
+        public ObservableCollection<string> Chats { get; } = new();
+        public ObservableCollection<string> Building { get; } = new();
+        public ObservableCollection<string> Position { get; } = new();
+        public ObservableCollection<string> Connection { get; } = new();
+
+        public ObservableCollection<string> KillsNoID { get; } = new();
+        public ObservableCollection<string> HitsNoID { get; } = new();
+        public ObservableCollection<string> ChatsNoID { get; } = new();
+        public ObservableCollection<string> BuildingNoID { get; } = new();
+        public ObservableCollection<string> PositionNoID { get; } = new();
+        public ObservableCollection<string> ConnectionNoID { get; } = new();
+
+        public ObservableCollection<string> KillsNoPos { get; } = new();
+        public ObservableCollection<string> HitsNoPos { get; } = new();
+        public ObservableCollection<string> BuildingNoPos { get; } = new();
+
+        public ObservableCollection<string> KillsNoIDNoPos { get; } = new();
+        public ObservableCollection<string> HitsNoIDNoPos { get; } = new();
+        public ObservableCollection<string> BuildingNoIDNoPos { get; } = new();
+
+        public void Parse(string fileName) {
+            var fileText = File.ReadAllLines(fileName);
+            foreach(var line in fileText) {
+                if(string.IsNullOrEmpty(line)) continue;
+                if(line.Contains("Chat")) {
+                    Chats.Add(line);
+                    var noID = RemoveID(line);
+                    ChatsNoID.Add(noID);
+                }
+                else if(line.Contains("connected") || line.Contains("disconnected")) {
+                    Connection.Add(line);
+                    var noID = RemoveID(line);
+                    ConnectionNoID.Add(noID);
+                }
+                else if(line.Contains("killed") || line.Contains("died") || line.Contains("suicide")) {
+                    Kills.Add(line);
+                    var noID = RemoveID(line);
+                    var noPos = RemovePos(line);
+                    KillsNoID.Add(noID);
+                    KillsNoPos.Add(noPos);
+                    KillsNoIDNoPos.Add(RemovePos(noID).Replace("()", ""));
+                }
+                else if(line.Contains("hit by") || line.Contains("conscious")) {
+                    Hits.Add(line);
+                    var noID = RemoveID(line);
+                    var noPos = RemovePos(line);
+                    HitsNoID.Add(noID);
+                    HitsNoPos.Add(noPos);
+                    HitsNoIDNoPos.Add(RemovePos(noID).Replace("()", ""));
+                }
+                else if(line.Contains("built") || line.Contains("placed") || line.Contains("deployed") ||
+                        line.Contains("packed")) {
+                    Building.Add(line);
+                    var noID = RemoveID(line);
+                    var noPos = RemovePos(line);
+                    BuildingNoID.Add(noID);
+                    BuildingNoPos.Add(noPos);
+                    BuildingNoIDNoPos.Add(RemovePos(noID).Replace("()", ""));
+                }
+                else if(line.Contains("Player ") || line.Contains("####")) {
+                    Position.Add(line);
+                    PositionNoID.Add(RemoveID(line));
+                }
+            }
+        }
+
+        private static string RemoveID(string input) {
+            Regex idRgx = new(@"\(id=[^) ]*\)|id=[^) ]* ");
+            var matches = idRgx.Matches(input);
+            return matches.Cast<Match>()
+                .Aggregate(input, (current, match) => current.Replace(match.Value, "").Replace("  ", " "));
+        }
+
+        private static string RemovePos(string input) {
+            Regex posRgx = new(@"pos=<[^>]*>");
+            var matches = posRgx.Matches(input);
+            return matches.Cast<Match>()
+                .Aggregate(input, (current, match) => current.Replace(match.Value, "").Replace("  ", " "));
+        }
+    }
+}
